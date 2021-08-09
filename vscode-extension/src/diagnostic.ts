4ebe51ff47d879f6a3aa50dc0143a45248381ebd
Zeng Li <t-zengli@microsoft.com>
2021-08-09 10:51:50 +0800

    refact: the diagnostic part

--- vscode-extension/src/diagnostic.ts
+++ vscode-extension/src/diagnostic.ts
@@ -16,30 +16,8 @@ export async function updateDiagnostics(
 			const planResult = await powershell.getUpgradePlan(documentUri.fsPath, azureRmVersion, azVersion);
 			if (planResult){
 				vscode.window.showInformationMessage("Node-Powershell Success!");
-				var plans = JSON.parse(planResult).forEach((plan : any, index : any) => {
-				//console.log(plan);
-				let range = new vscode.Range(new vscode.Position(plan.SourceCommand.StartLine - 1, plan.SourceCommand.StartColumn - 1), 
-													new vscode.Position(plan.SourceCommand.EndLine - 1, plan.SourceCommand.EndPosition - 1));
-				let message = plan.PlanResultReason;
-				let diagnostic = new vscode.Diagnostic(range, message);
-				if (plan.PlanSeverity == 1){
-					diagnostic.severity = vscode.DiagnosticSeverity.Error;
-					diagnostic.code = "DO_NOTHING";
-					diagnostic.source = '';
-				}
-				else if (plan.PlanSeverity == 2){
-					diagnostic.severity = vscode.DiagnosticSeverity.Information;
-					diagnostic.code = "DO_NOTHING";
-					diagnostic.source = '';
-				}
-				else{	//plan.PlanSeverity == 3
-					diagnostic.severity = vscode.DiagnosticSeverity.Warning;
-					diagnostic.code = "RENAME";
-					diagnostic.source = plan.Replacement;
-				}
-				diagnostics.push(diagnostic);
-			});
-			collection.set(documentUri, diagnostics);
+				updateDiagnosticsMessage(planResult, diagnostics);
+				collection.set(documentUri, diagnostics);
 			}
 			else{
 				var process = require("child_process");
@@ -51,32 +29,8 @@ export async function updateDiagnostics(
 					}
 					else
 						vscode.window.showInformationMessage("Process Success!");
-					if (stdout != null){
-						var plans = JSON.parse(stdout).forEach((plan : any, index : any) => {
-							//console.log(plan);
-							let range = new vscode.Range(new vscode.Position(plan.SourceCommand.StartLine - 1, plan.SourceCommand.StartColumn - 1), 
-																new vscode.Position(plan.SourceCommand.EndLine - 1, plan.SourceCommand.EndPosition - 1));
-							let message = plan.PlanResultReason;
-							let diagnostic = new vscode.Diagnostic(range, message);
-							if (plan.PlanSeverity == 1){
-								diagnostic.severity = vscode.DiagnosticSeverity.Error;
-								diagnostic.code = "DO_NOTHING";
-								diagnostic.source = '';
-							}
-							else if (plan.PlanSeverity == 2){
-								diagnostic.severity = vscode.DiagnosticSeverity.Information;
-								diagnostic.code = "DO_NOTHING";
-								diagnostic.source = '';
-							}
-							else{	//plan.PlanSeverity == 3
-								diagnostic.severity = vscode.DiagnosticSeverity.Warning;
-								diagnostic.code = "RENAME";
-								diagnostic.source = plan.Replacement;
-							}
-							diagnostics.push(diagnostic);
-						});
-						
-					}	
+					if (stdout != null)
+						updateDiagnosticsMessage(stdout, diagnostics);	
 					collection.set(documentUri, diagnostics);
 				});
 			}
@@ -87,3 +41,30 @@ export async function updateDiagnostics(
 		collection.clear();
 	}
 }
+
+function updateDiagnosticsMessage(plansStr : string, diagnostics : vscode.Diagnostic[]){
+	var plans = JSON.parse(plansStr).forEach((plan : any, index : any) => {
+		//console.log(plan);
+		let range = new vscode.Range(new vscode.Position(plan.SourceCommand.StartLine - 1, plan.SourceCommand.StartColumn - 1), 
+											new vscode.Position(plan.SourceCommand.EndLine - 1, plan.SourceCommand.EndPosition - 1));
+		let message = plan.PlanResultReason;
+		let diagnostic = new vscode.Diagnostic(range, message);
+		if (plan.PlanSeverity == 1){
+			diagnostic.severity = vscode.DiagnosticSeverity.Error;
+			diagnostic.code = "DO_NOTHING";
+			diagnostic.source = '';
+		}
+		else if (plan.PlanSeverity == 2){
+			diagnostic.severity = vscode.DiagnosticSeverity.Information;
+			diagnostic.code = "DO_NOTHING";
+			diagnostic.source = '';
+		}
+		else{	//plan.PlanSeverity == 3
+			diagnostic.severity = vscode.DiagnosticSeverity.Warning;
+			diagnostic.code = "RENAME";
+			diagnostic.source = plan.Replacement;
+		}
+		diagnostics.push(diagnostic);
+	});
+}
+
